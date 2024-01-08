da2758b7fbea7c67bec1e96743596c0ec3ea7ca9
sada-shukurova <shukurova.sada@gmail.com>
2024-01-08 17:28:00 +0400

    add Auth0

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-import Nav from "./comp/Nav";
+import Nav from "./components/Nav";
 
 function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+// auth
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <Auth0Provider
+      domain="dev-4n7kiqk1w1koa2g3.us.auth0.com"
+      clientId="C6gKbI2NmXmcnPcRDmGH8nsgIkXLwDns"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </>
 );
