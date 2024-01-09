b01183948e7c10df2afa965b40b648ca8941841b
sada-shukurova <shukurova.sada@gmail.com>
2024-01-09 11:49:24 +0400

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { BrowserRouter } from "react-router-dom";
-import Nav from "./components/Nav";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav/Nav";
+import myRoutes from "./myRoutes";
 
 function App() {
   return (
     <BrowserRouter>
       <Nav />
+      <Routes>
+        {myRoutes.map(({ id, to, element }) => (
+          <Route path={to} element={element} key={id} />
+        ))}
+      </Routes>
     </BrowserRouter>
   );
 }
